301e9e5c2a0f1cd605a9ec4e8910c028d3d596d8
Fletcher <fletcherc89@gmail.com>
2016-01-30 02:11:59 -0800

    Zoom on phone when looking down

---
+++ Assets/GGJ2016/Scripts/PhoneViewing/ZoomOnPhone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ZoomOnPhone : MonoBehaviour
+{
+	public float RotationThreshold = 40.0f;
+	public float ZoomFOV = 40;
+	
+	private float _normalFOV;
+	private Camera _camera;
+	
+	void Start()
+	{
+		_camera = GetComponent<Camera>();
+		_normalFOV = _camera.fieldOfView;
+	}
+	
+	void Update()
+	{
+		if (this.transform.localRotation.eulerAngles.x >= this.RotationThreshold)
+		{
+			_camera.fieldOfView = this.ZoomFOV;
+		}
+		else
+		{
+			_camera.fieldOfView = _normalFOV;
+		}
+	}
+}
