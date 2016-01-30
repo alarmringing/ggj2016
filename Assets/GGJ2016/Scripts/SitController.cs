f22feb7f2a16b7af17c589ae929e9f10c6ef5a2b
alarmringing <alarmringing@gmail.com>
2016-01-30 05:32:44 -0800

    updated scenes and scripts on home and street01, added streetSF asset

---
+++ Assets/GGJ2016/Scripts/SitController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SitController : MonoBehaviour {
+	
+	public GameObject chairObj;
+	public GameObject getupObj;
+	Vector3 doorSpawnPoint;
+	Vector3 bedSpawnPoint;
+	
+	// Use this for initialization
+	void Sit()
+	{
+		
+	}
+}
