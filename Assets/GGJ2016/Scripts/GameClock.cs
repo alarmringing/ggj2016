f8f39e7221785626d482a9511b455e17ba7da596
Alex Peake <alex@primerlabs.com>
2016-01-30 05:35:08 -0800

    added clock and ui

--- Assets/GGJ2016/Scripts/GameClock.cs
+++ Assets/GGJ2016/Scripts/GameClock.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class GameClock : MonoBehaviour {
 	
 	public System.DateTime dateTime;
+	public Text clockText;
 	
 	public string dateTimeClockString = "XX:XX PM";
 	// Use this for initialization
@@ -14,6 +16,8 @@ public class GameClock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		dateTimeClockString = dateTime.ToShortTimeString();
+		dateTime = System.DateTime.Now;
+		dateTimeClockString = dateTime.ToLongTimeString() + " " + dateTime.ToString("tt", System.Globalization.CultureInfo.InvariantCulture);
+		clockText.text = dateTimeClockString;
 	}
 }
---
+++ Assets/GGJ2016/Scripts/VRCharacter/Seating.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seating : MonoBehaviour {
+	
+	public bool seated = false;
+	public GameObject player;
+	public Vector3 standingPosition;
+	public Transform seatedPositionTransform;
+	public ActivateTriggerCrosshair sitTrigger;
+	public ActivateTriggerCrosshair standTrigger;
+	
+	public void Sit()
+	{
+		Debug.Log("Sitting");
+		seated = true;
+		standingPosition = player.transform.position;
+		CharacterController cc = player.GetComponent<CharacterController>();
+		cc.height = 1f;
+		player.transform.position = seatedPositionTransform.position;
+		sitTrigger.on = false;
+		standTrigger.on = true;
+		cc.enabled=false;
+		
+		
+	}
+	
+	public void Stand()
+	{
+		CharacterController cc = player.GetComponent<CharacterController>();
+		cc.height = 1.8f;
+		cc.enabled=true;
+		seated = false;
+		player.transform.position = standingPosition;
+		sitTrigger.on = true;
+		standTrigger.on = false;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(seated)
+		{
+			player.transform.position = seatedPositionTransform.position;
+			if(Input.GetAxis("Vertical") < 0 )
+			{
+				Stand();
+			}
+		}
+	}
+}
