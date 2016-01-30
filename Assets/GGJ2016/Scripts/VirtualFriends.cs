fe77e5676ac3078da36796997cc94e37ed1cf8a7
Alex Peake <alex@primerlabs.com>
2016-01-30 08:51:36 -0800

    Messages working better in Caltrain Station Clock scene

--- Assets/GGJ2016/Scripts/VirtualFriends.cs
+++ Assets/GGJ2016/Scripts/VirtualFriends.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class VirtualFriends : MonoBehaviour {
 	
@@ -30,12 +31,23 @@ public class VirtualFriends : MonoBehaviour {
 	public List<VirtualFriend> realFriends;
 	public List<VirtualFriend> gameFriends;
 	
+	public enum Mode {
+		Home,
+		Messages,
+		Message,
+		Game
+	}
+	
+	Mode mode = Mode.Home;
+	
+	
 	
 	[System.Serializable]
 	public class VirtualMessage
 	{
 		public float timeReceived = 10f;
 		public bool received = false;
+		public bool read = false;
 		public bool helped = false;
 		public string VirtualFriendName = "Tom";
 		public string messageSubject = "Hi";
@@ -75,6 +87,12 @@ public class VirtualFriends : MonoBehaviour {
 		nextMessage=null;
 		float soonestMessageTime = 0f;
 		VirtualMessage soonestMessage = null;
+		if ( futureMessages!=null && futureMessages.Count>0)
+		{
+		nextMessage = futureMessages[0];
+			nextMessageTime = nextMessage.timeReceived;
+		}
+		/*
 		foreach (VirtualMessage vm in futureMessages)
 		{
 			// If this message is sooner than the soonest previously seen
@@ -88,7 +106,9 @@ public class VirtualFriends : MonoBehaviour {
 		{
 			nextMessage = soonestMessage;
 			nextMessageTime = soonestMessageTime;
+			Debug.Log("Next message subject is " + nextMessage.messageSubject);
 		}
+		*/
 	}
 	
 	public AudioClip messageReceivedClip;
@@ -96,13 +116,20 @@ public class VirtualFriends : MonoBehaviour {
 	public void MessageNotification()
 	{
 		gameObject.GetComponent<AudioSource>().PlayOneShot(messageReceivedClip);
+		unreadMessages++;
+		unreadMessagesText.text = unreadMessages+"";
+		if (mode == Mode.Messages)
+		{
+			MenubarButton();
+		}
 	}
 	
+	public Text unreadMessagesText;
 	
+	public int unreadMessages = 0;
 
 	// Use this for initialization
 	void Start () {
-		nextMessage=null;
 		DetermineNextMessage();
 	}
 	
@@ -115,21 +142,86 @@ public class VirtualFriends : MonoBehaviour {
 	public GameObject messageUICanvas;
 	public GameObject gameUICanvas;
 	
+	public GridLayoutGroup buttonGridLayoutGroup;
+	public GameObject messageButtonTemplate;
+	
 	public void MenubarButton()
 	{
+		mode = Mode.Messages;
+		if(buttonGridLayoutGroup!=null)
+		{
+			Button[] buttons = buttonGridLayoutGroup.GetComponentsInChildren<Button>();
+			foreach (Button b in buttons)
+			{
+				GameObject.Destroy(b.gameObject);
+			}
+			int i = 0;
+			foreach (VirtualMessage vm in inbox)
+			{
+				GameObject newButton = GameObject.Instantiate(messageButtonTemplate);
+				newButton.SetActive(true);
+				newButton.transform.SetParent(buttonGridLayoutGroup.transform);
+				newButton.gameObject.GetComponentInChildren<Text>().text = vm.messageSubject;
+				AddButtonListenerInt(newButton.GetComponent<Button>(),i);
+				i++;
+			}
+		}
 		messagesUICanvas.SetActive(true);
 		gameUICanvas.SetActive(false);
 		messageUICanvas.SetActive(false);
 	}
 	
-	public void MessageButton()
+	/// <summary>
+	/// Necessary to avoid the foreach loop only using the last value. See http://answers.unity3d.com/questions/791573/46-ui-how-to-apply-onclick-handler-for-button-gene.html
+	/// </summary>
+	/// <param name="b"></param>
+	/// <param name="intArg"></param>
+	public void AddButtonListenerInt(Button b, int intArg)
 	{
-		messagesUICanvas.SetActive(false);
-		gameUICanvas.SetActive(false);
-		messageUICanvas.SetActive(true);
+		b.onClick.AddListener( () => { MessageButton(intArg); } );
+	}
+	
+	private VirtualMessage displayedMessage;
+	public int displayedMessageIndex = 0;
+	
+	public Text fromText;
+	public Text subjectText;
+	public Text bodyText;
+	public RawImage portraitImage;
+	
+	public void MessageButton(int messageIndex)
+	{
+		Debug.Log("MessageButton()");
+		if(inbox.Count >= messageIndex+1)
+		{
+			mode = Mode.Message;
+		
+			displayedMessage = inbox[messageIndex];
+		
+			fromText.text = displayedMessage.VirtualFriendName;
+			subjectText.text = displayedMessage.messageSubject;
+			bodyText.text = displayedMessage.messageText;
+			//portraitImage.texture = displayedMessage. 
+			// Need to have a portrait getter for a given name
+			
+			if (!displayedMessage.read)
+			{
+				displayedMessage.read = true;
+				unreadMessages--;
+				UnityEngine.Debug.Log("unreadMessages--");
+			}
+			messagesUICanvas.SetActive(false);
+			gameUICanvas.SetActive(false);
+			messageUICanvas.SetActive(true);
+		}
+		else
+		{
+			Debug.Log("inbox["+messageIndex+"] !>= messageIndex+1");
+		}
 	}
 	public void GameButton()
 	{
+		mode = Mode.Game;
 		messagesUICanvas.SetActive(false);
 		gameUICanvas.SetActive(true);
 		messageUICanvas.SetActive(false);
