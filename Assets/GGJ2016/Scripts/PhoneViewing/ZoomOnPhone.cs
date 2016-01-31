bca9c5b30f08eb34e65335a96cc1c22fa3dac946
Fletcher <fletcherc89@gmail.com>
2016-01-30 22:40:43 -0800

    Better lighting on phone screen, two phone positions, disable crosshairs when using phone

--- Assets/GGJ2016/Scripts/PhoneViewing/ZoomOnPhone.cs
+++ Assets/GGJ2016/Scripts/PhoneViewing/ZoomOnPhone.cs
@@ -5,6 +5,9 @@ public class ZoomOnPhone : MonoBehaviour
 	public float RotationThreshold = 40.0f;
     public float RotationMaxThreshold = 120.0f;
     public float RotateBackThreshold = 15.0f;
+    public Transform PhoneZoomedTransform;
+    public Transform PhoneAtSideTransform;
+    public GameObject PhoneObject;
 	public float ZoomFOV = 40;
 	
 	public bool IsZoomed { get { return _zoomed; }}
@@ -12,10 +15,12 @@ public class ZoomOnPhone : MonoBehaviour
 	private float _normalFOV;
 	private Camera _camera;
 	private bool _zoomed;
+    private Crosshair _crosshair;
 	
 	void Start()
 	{
 		_camera = GetComponent<Camera>();
+        _crosshair = GetComponent<Crosshair>();
 		_normalFOV = _camera.fieldOfView;
 	}
 	
@@ -26,11 +31,17 @@ public class ZoomOnPhone : MonoBehaviour
 		{
 			_camera.fieldOfView = this.ZoomFOV;
 			_zoomed = true;
-		}
+            this.PhoneObject.transform.position = this.PhoneZoomedTransform.position;
+            this.PhoneObject.transform.rotation = this.PhoneZoomedTransform.rotation;
+            _crosshair.Enable(false);
+        }
 		else if (_zoomed && euler.x <= this.RotateBackThreshold)
 		{
 			_camera.fieldOfView = _normalFOV;
 			_zoomed = false;
-		}
+            this.PhoneObject.transform.position = this.PhoneAtSideTransform.position;
+            this.PhoneObject.transform.rotation = this.PhoneAtSideTransform.rotation;
+            _crosshair.Enable(true);
+        }
 	}
 }
