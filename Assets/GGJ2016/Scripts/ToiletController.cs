c0cd55e20f21caac0bfcebc6f95406895948a91e
alarmringing <alarmringing@gmail.com>
2016-01-30 16:10:50 -0800

    Added iPad in home scene, modified toilet / seating script, added warp points

---
+++ Assets/GGJ2016/Scripts/ToiletController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletController : Seating{
+
+	float startTime;
+	float useToiletTime = 30.0f;
+
+	// Use this for initialization
+	public void SitOnToilet () {
+
+		startTime = Time.time;
+
+		Debug.Log("SatonToilet. startTime updated.");
+
+		Debug.Log("Sit");
+		Sit();		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(seated == true && Time.time > (startTime + useToiletTime))
+		{
+			Stand();
+			Debug.Log("Stood up");
+			GetComponent<AudioSource>().Play();	
+		}
+
+	}
+}
